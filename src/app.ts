a800ac98702ad6907246287ba83b32eca4a8cd58
seungh0 <will.seungho@gmail.com>
2021-01-30 15:11:17 +0900

    feat(setup): setup project

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+export default class App {
+  private app: express.Application;
+
+  constructor() {
+    this.app = express();
+  }
+
+  public async runServer(port: number): Promise<void> {
+    try {
+      this.app.listen(port, () => {
+        console.log(`
+              ################################################
+              🛡️  Server listening on port: ${port}
+              ################################################
+              `);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
---
+++ src/www.ts
@@ -0,0 +1,9 @@
+import App from "./app";
+
+try {
+  const app = new App();
+  const port: number = 8000;
+  app.runServer(port);
+} catch (error) {
+  console.error(error);
+}
