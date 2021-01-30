683c0c1354a431845d3ad0b602ee9562dec94fa4
seungh0 <will.seungho@gmail.com>
2021-01-30 19:28:30 +0900

    production ormconfig 설정

--- ormconfig.ts
+++ ormconfig.ts
@@ -11,6 +11,17 @@ export default [
     database: config.database.database,
     entities: ['./src/domains/*/*.entity{.ts,.js}'],
     synchronize: true,
+    logging: false,
+  },
+  {
+    name: 'prod',
+    type: config.database.dialect,
+    host: config.database.host,
+    port: config.database.port,
+    username: config.database.username,
+    password: config.database.password,
+    database: config.database.database,
+    entities: ['./src/domains/*/*.entity{.ts,.js}'],
     logging: true,
   },
 ];
